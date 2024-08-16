dee18ebc9b5394ea0a95c06431e38b52e9cc23ca
uy_sun <hmy0119@gmail.com>
2024-08-16 12:01:27 +0800

    docs: 完善插件元数据中的 usage

--- nonebot_plugin_memes/__init__.py
+++ nonebot_plugin_memes/__init__.py
@@ -14,7 +14,16 @@ from .config import Config
 __plugin_meta__ = PluginMetadata(
     name="表情包制作",
     description="制作各种沙雕表情包",
-    usage="发送“表情包制作”查看表情包列表",
+    usage=(
+        "发送 “表情包制作” 查看表情列表\n"
+        "发送 “表情详情 + 表情名/关键词” 查看表情详细信息和表情预览\n"
+        "发送 “表情搜索 + 关键词” 查找相关的表情\n\n"
+        "“超级用户” 和 “管理员” 可以启用或禁用某些表情包\n"
+        "发送 启用表情/禁用表情 表情名/关键词，如：禁用表情 摸\n"
+        "“超级用户” 可以设置某个表情包的管控模式（黑名单/白名单）\n"
+        "发送 全局启用表情 表情名/关键词 可将表情设为黑名单模式；\n"
+        "发送 全局禁用表情 表情名/关键词 可将表情设为白名单模式；"
+    ),
     type="application",
     homepage="https://github.com/noneplugin/nonebot-plugin-memes",
     config=Config,
